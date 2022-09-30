aeb5fa530108d3feff4c893e477bc7a5690d6c42
Thomas Lenzmeier <thomas.lenzmeier@constellationmutual.com>
2022-09-30 14:53:45 -0500

    Create the bar graph.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,46 @@
 console.log('This is app.js')
+// Define a global variable
+
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 function DrawBargraph(sampleID)
 {
     console.log(`DrawBargraph(${sampleID})`);
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleID);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+        // Create a trace object, put it into an array, call Plotly
+        let barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+
+        };
+        // Create a trace object
+        let barArray = [barData];
+        // Create a layout object
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+        // Call the Plotly function
+        Plotly.newPlot("bar", barArray, barLayout);
+
+
+    })
 }
 
 function DrawBubblechart(sampleID)
